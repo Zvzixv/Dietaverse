32704c2dea818dcfc88282f8ec81c1f1b88b73b6
Zvzixv <zuziapudo@icloud.com>
2022-07-17 14:29:27 +0200

    startowy panel wraz z guzikami

---
+++ Dietaverse/Dietaverse/View/Start.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Dietaverse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.monthButton = new System.Windows.Forms.Button();
+            this.smoothiesButton = new System.Windows.Forms.Button();
+            this.foodGalleryButton = new System.Windows.Forms.Button();
+            this.bodyGalleryButton = new System.Windows.Forms.Button();
+            this.dashboardButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PnlNav = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.PnlNav);
+            this.panel1.Controls.Add(this.settingsButton);
+            this.panel1.Controls.Add(this.monthButton);
+            this.panel1.Controls.Add(this.smoothiesButton);
+            this.panel1.Controls.Add(this.foodGalleryButton);
+            this.panel1.Controls.Add(this.bodyGalleryButton);
+            this.panel1.Controls.Add(this.dashboardButton);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settingsButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingsButton.ForeColor = System.Drawing.Color.Coral;
+            this.settingsButton.Location = new System.Drawing.Point(0, 521);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(186, 56);
+            this.settingsButton.TabIndex = 1;
+            this.settingsButton.Text = "Settings";
+            this.settingsButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            this.settingsButton.Leave += new System.EventHandler(this.settingsButton_Leave);
+            // 
+            // monthButton
+            // 
+            this.monthButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.monthButton.FlatAppearance.BorderSize = 0;
+            this.monthButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.monthButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthButton.ForeColor = System.Drawing.Color.Coral;
+            this.monthButton.Image = ((System.Drawing.Image)(resources.GetObject("monthButton.Image")));
+            this.monthButton.Location = new System.Drawing.Point(0, 328);
+            this.monthButton.Name = "monthButton";
+            this.monthButton.Size = new System.Drawing.Size(186, 51);
+            this.monthButton.TabIndex = 1;
+            this.monthButton.Text = "Month summary";
+            this.monthButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.monthButton.UseVisualStyleBackColor = true;
+            this.monthButton.Click += new System.EventHandler(this.monthButton_Click);
+            this.monthButton.Leave += new System.EventHandler(this.monthButton_Leave);
+            // 
+            // smoothiesButton
+            // 
+            this.smoothiesButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smoothiesButton.FlatAppearance.BorderSize = 0;
+            this.smoothiesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smoothiesButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.smoothiesButton.ForeColor = System.Drawing.Color.Coral;
+            this.smoothiesButton.Image = ((System.Drawing.Image)(resources.GetObject("smoothiesButton.Image")));
+            this.smoothiesButton.Location = new System.Drawing.Point(0, 277);
+            this.smoothiesButton.Name = "smoothiesButton";
+            this.smoothiesButton.Size = new System.Drawing.Size(186, 51);
+            this.smoothiesButton.TabIndex = 1;
+            this.smoothiesButton.Text = "Smoothies";
+            this.smoothiesButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.smoothiesButton.UseVisualStyleBackColor = true;
+            this.smoothiesButton.Click += new System.EventHandler(this.smoothiesButton_Click);
+            this.smoothiesButton.Leave += new System.EventHandler(this.smoothiesButton_Leave);
+            // 
+            // foodGalleryButton
+            // 
+            this.foodGalleryButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.foodGalleryButton.FlatAppearance.BorderSize = 0;
+            this.foodGalleryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.foodGalleryButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.foodGalleryButton.ForeColor = System.Drawing.Color.Coral;
+            this.foodGalleryButton.Image = ((System.Drawing.Image)(resources.GetObject("foodGalleryButton.Image")));
+            this.foodGalleryButton.Location = new System.Drawing.Point(0, 226);
+            this.foodGalleryButton.Name = "foodGalleryButton";
+            this.foodGalleryButton.Size = new System.Drawing.Size(186, 51);
+            this.foodGalleryButton.TabIndex = 1;
+            this.foodGalleryButton.Text = "Food gallery";
+            this.foodGalleryButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.foodGalleryButton.UseVisualStyleBackColor = true;
+            this.foodGalleryButton.Click += new System.EventHandler(this.foodGalleryButton_Click);
+            this.foodGalleryButton.Leave += new System.EventHandler(this.foodGalleryButton_Leave);
+            // 
+            // bodyGalleryButton
+            // 
+            this.bodyGalleryButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bodyGalleryButton.FlatAppearance.BorderSize = 0;
+            this.bodyGalleryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bodyGalleryButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bodyGalleryButton.ForeColor = System.Drawing.Color.Coral;
+            this.bodyGalleryButton.Image = ((System.Drawing.Image)(resources.GetObject("bodyGalleryButton.Image")));
+            this.bodyGalleryButton.Location = new System.Drawing.Point(0, 175);
+            this.bodyGalleryButton.Name = "bodyGalleryButton";
+            this.bodyGalleryButton.Size = new System.Drawing.Size(186, 51);
+            this.bodyGalleryButton.TabIndex = 1;
+            this.bodyGalleryButton.Text = "Body gallery";
+            this.bodyGalleryButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.bodyGalleryButton.UseVisualStyleBackColor = true;
+            this.bodyGalleryButton.Click += new System.EventHandler(this.bodyGalleryButton_Click);
+            this.bodyGalleryButton.Leave += new System.EventHandler(this.bodyGalleryButton_Leave);
+            // 
+            // dashboardButton
+            // 
+            this.dashboardButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dashboardButton.FlatAppearance.BorderSize = 0;
+            this.dashboardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardButton.ForeColor = System.Drawing.Color.Coral;
+            this.dashboardButton.Image = ((System.Drawing.Image)(resources.GetObject("dashboardButton.Image")));
+            this.dashboardButton.Location = new System.Drawing.Point(0, 124);
+            this.dashboardButton.Name = "dashboardButton";
+            this.dashboardButton.Size = new System.Drawing.Size(186, 51);
+            this.dashboardButton.TabIndex = 1;
+            this.dashboardButton.Text = "Dashboard";
+            this.dashboardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.dashboardButton.UseVisualStyleBackColor = true;
+            this.dashboardButton.Click += new System.EventHandler(this.dashboardButton_Click);
+            this.dashboardButton.Leave += new System.EventHandler(this.dashboardButton_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 124);
+            this.panel2.TabIndex = 0;
+            // 
+            // PnlNav
+            // 
+            this.PnlNav.BackColor = System.Drawing.Color.Coral;
+            this.PnlNav.Location = new System.Drawing.Point(0, 193);
+            this.PnlNav.Name = "PnlNav";
+            this.PnlNav.Size = new System.Drawing.Size(3, 100);
+            this.PnlNav.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(45, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Coral;
+            this.label2.Location = new System.Drawing.Point(213, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dashboard";
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Location = new System.Drawing.Point(914, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(25, 25);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button settingsButton;
+        private System.Windows.Forms.Button monthButton;
+        private System.Windows.Forms.Button smoothiesButton;
+        private System.Windows.Forms.Button foodGalleryButton;
+        private System.Windows.Forms.Button bodyGalleryButton;
+        private System.Windows.Forms.Button dashboardButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel PnlNav;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Dietaverse/Dietaverse/View/Start.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Dietaverse
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint ="CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidrhElipse, int nHeightEllipse);
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            PnlNav.Height = dashboardButton.Height;
+            PnlNav.Top = dashboardButton.Top;
+            PnlNav.Left = dashboardButton.Left;
+            dashboardButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dashboardButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = dashboardButton.Height;
+            PnlNav.Top = dashboardButton.Top;
+            PnlNav.Left = dashboardButton.Left;
+            dashboardButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void bodyGalleryButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = bodyGalleryButton.Height;
+            PnlNav.Top = bodyGalleryButton.Top;
+            PnlNav.Left = bodyGalleryButton.Left;
+            bodyGalleryButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void foodGalleryButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = foodGalleryButton.Height;
+            PnlNav.Top = foodGalleryButton.Top;
+            PnlNav.Left = foodGalleryButton.Left;
+            foodGalleryButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void smoothiesButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = smoothiesButton.Height;
+            PnlNav.Top = smoothiesButton.Top;
+            PnlNav.Left = smoothiesButton.Left;
+            smoothiesButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void monthButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = monthButton.Height;
+            PnlNav.Top = monthButton.Top;
+            PnlNav.Left = monthButton.Left;
+            monthButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            PnlNav.Height = settingsButton.Height;
+            PnlNav.Top = settingsButton.Top;
+            PnlNav.Left = settingsButton.Left;
+            settingsButton.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void dashboardButton_Leave(object sender, EventArgs e)
+        {
+            dashboardButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void bodyGalleryButton_Leave(object sender, EventArgs e)
+        {
+            bodyGalleryButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void foodGalleryButton_Leave(object sender, EventArgs e)
+        {
+            foodGalleryButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void smoothiesButton_Leave(object sender, EventArgs e)
+        {
+            smoothiesButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void monthButton_Leave(object sender, EventArgs e)
+        {
+            monthButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void settingsButton_Leave(object sender, EventArgs e)
+        {
+            settingsButton.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
