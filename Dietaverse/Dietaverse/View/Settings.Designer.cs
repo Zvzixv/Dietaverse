25a255b7f10893fd85967cf0bf6e1592a5d7fb41
Zvzixv <zuziapudo@icloud.com>
2022-10-23 17:46:14 +0200

    zmiana hasla nie dziala

--- Dietaverse/Dietaverse/View/Settings.Designer.cs
+++ Dietaverse/Dietaverse/View/Settings.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.logoutbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pass1textBox = new System.Windows.Forms.TextBox();
+            this.pass2textBox = new System.Windows.Forms.TextBox();
+            this.changePasswordbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // logoutbutton
@@ -44,22 +48,77 @@
             this.logoutbutton.UseVisualStyleBackColor = true;
             this.logoutbutton.Click += new System.EventHandler(this.logoutbutton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(33, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Change password";
+            // 
+            // pass1textBox
+            // 
+            this.pass1textBox.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pass1textBox.Location = new System.Drawing.Point(37, 161);
+            this.pass1textBox.Name = "pass1textBox";
+            this.pass1textBox.Size = new System.Drawing.Size(146, 21);
+            this.pass1textBox.TabIndex = 13;
+            this.pass1textBox.Text = "Insert new password";
+            this.pass1textBox.Leave += new System.EventHandler(this.pass1textBox_Leave);
+            this.pass1textBox.Validated += new System.EventHandler(this.pass1textBox_Validated);
+            // 
+            // pass2textBox
+            // 
+            this.pass2textBox.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pass2textBox.Location = new System.Drawing.Point(37, 187);
+            this.pass2textBox.Name = "pass2textBox";
+            this.pass2textBox.Size = new System.Drawing.Size(146, 21);
+            this.pass2textBox.TabIndex = 13;
+            this.pass2textBox.Text = "Repeat new password";
+            this.pass2textBox.Leave += new System.EventHandler(this.pass2textBox_Leave);
+            this.pass2textBox.Validated += new System.EventHandler(this.pass2textBox_Validated);
+            // 
+            // changePasswordbutton
+            // 
+            this.changePasswordbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.changePasswordbutton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.changePasswordbutton.ForeColor = System.Drawing.Color.Coral;
+            this.changePasswordbutton.Location = new System.Drawing.Point(37, 228);
+            this.changePasswordbutton.Name = "changePasswordbutton";
+            this.changePasswordbutton.Size = new System.Drawing.Size(109, 38);
+            this.changePasswordbutton.TabIndex = 11;
+            this.changePasswordbutton.Text = "Apply ";
+            this.changePasswordbutton.UseVisualStyleBackColor = true;
+            this.changePasswordbutton.Click += new System.EventHandler(this.logoutbutton_Click);
+            // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(733, 477);
+            this.Controls.Add(this.pass2textBox);
+            this.Controls.Add(this.pass1textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.changePasswordbutton);
             this.Controls.Add(this.logoutbutton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Settings";
             this.Text = "Settings";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button logoutbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pass1textBox;
+        private System.Windows.Forms.TextBox pass2textBox;
+        private System.Windows.Forms.Button changePasswordbutton;
     }
 }
--- Dietaverse/Dietaverse/View/Settings.cs
+++ Dietaverse/Dietaverse/View/Settings.cs
@@ -13,6 +13,8 @@ namespace Dietaverse.View
     public partial class Settings : Form
     {
         mainForm mainform;
+        bool pass1wasclicked = false;
+        bool pass2wasclicked = false;
         public Settings(mainForm mf)
         {
             InitializeComponent();
@@ -23,6 +25,51 @@ namespace Dietaverse.View
         {
             Launch launchform = new Launch(mainform) { Dock = DockStyle.Fill, TopLevel = false, TopMost = true, FormBorderStyle = FormBorderStyle.None };
             mainform.changeForm(launchform);
+        }
+
+        private void pass1textBox_Validated(object sender, EventArgs e)
+        {
+            if(!pass1wasclicked)
+            {
+                pass1textBox.Text = "";
+                pass1textBox.PasswordChar = '*';
+                pass1textBox.UseSystemPasswordChar = true;
+                pass1wasclicked = true;
+
+            }
+        }
+
+        private void pass1textBox_Leave(object sender, EventArgs e)
+        {
+            if(pass1wasclicked && pass1textBox.TextLength == 0 )
+            {
+                pass1textBox.Text = "Insert new password";
+                pass1wasclicked = false;
+            }
+
+        }
+
+        private void pass2textBox_Validated(object sender, EventArgs e)
+        {
+            if (!pass2wasclicked)
+            {
+                pass2textBox.Text = "";
+                pass2textBox.PasswordChar = '*';
+                pass2textBox.UseSystemPasswordChar = true;
+                pass2wasclicked = true;
+
+            }
+
+        }
+
+        private void pass2textBox_Leave(object sender, EventArgs e)
+        {
+            if (pass2wasclicked && pass2textBox.TextLength == 0)
+            {
+                pass2textBox.Text = "Insert new password";
+                pass2wasclicked = false;
+            }
+
         }
     }
 }
