4f02021412684beeead2a7811bc830664500a39d
Zvzixv <zuziapudo@icloud.com>
2022-10-26 21:01:43 +0200

    napoczete testy do smothies

---
+++ Dietaverse/DietaverseTests/Smoothies_recipesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietaverseTests
+{
+    internal class Smoothies_recipesTests
+    {
+        public void addNewSmoothie_GivenProperVariables_CreateSucceed()
+        {
+
+        }
+
+        public void makeAList_GivenList_CreateSucceed()
+        {
+
+        }
+
+        public void makeAList_GivenEmptyList_EmptyListReturned()
+        {
+
+        }
+    }
+}
