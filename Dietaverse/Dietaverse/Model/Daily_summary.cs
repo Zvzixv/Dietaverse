cb075ad973a22aac4e52e01f86d522d82e07dafa
Zvzixv <zuziapudo@icloud.com>
2022-11-07 20:00:28 +0100

    testy finish

--- Dietaverse/Dietaverse/Model/Daily_summary.cs
+++ Dietaverse/Dietaverse/Model/Daily_summary.cs
@@ -36,22 +36,6 @@ namespace Dietaverse.Model
             }
         }
 
-        public void create(double _weight, int _kcal, users _u, string _note, DateTime date)
-        {
-            using (var db = new db_modelContainer())
-            {
-                var u = db.usersSet;
-                daily_summary daily_Summary = new daily_summary();
-                daily_Summary.kcal_amount = _kcal;
-                daily_Summary.weight = _weight;
-                daily_Summary.notes = _note;
-                daily_Summary.date = date;
-                daily_Summary.users = u.Single(a => a.name == _u.name);
-                db.daily_summarySet.Add(daily_Summary);
-                db.SaveChanges();
-            }
-        }
-
         public void update(double _weight, int _kcal, users _u, string _note)
         {
             using (var db = new db_modelContainer())
@@ -152,67 +136,5 @@ namespace Dietaverse.Model
             }
         }
 
-        //kazdy dzien ktory nie jest zapisany ma miec dane z ostatniego zapisanego dnia (kilogramy) i zero kalorii 
-
-        //mozliwe, ze zbedne
-        //public void fillOtherDays(users us)
-        //{
-        //    using (var db = new db_modelContainer())
-        //    {
-        //        var users = db.usersSet;
-        //        var summaries = db.daily_summarySet;
-
-        //        DateTime start = new DateTime(2022, 9, 1, 0, 0, 0);
-        //        DateTime today = DateTime.Now;
-        //        daily_summary lastSaved = null;
-
-        //        TimeSpan difference = today - start;
-        //        int differenceInDays = (int)difference.TotalDays;
-
-        //        daily_summary searchedsummary = null;
-
-
-        //        cos jest nie tak z petla. Tworzy sie dzien ale tylko jeden meh
-
-        //                for (int i = 0; i < differenceInDays; i++)
-        //        {
-        //            try
-        //            {
-        //                //jest ten dzien?
-        //                searchedsummary = summaries.Single(a => a.users.name == us.name && a.date.Day == start.Day && a.date.Month == start.Month && a.date.Year == start.Year);
-        //            }
-
-        //            catch (Exception ex)
-        //            {
-        //                //nie ma tego dnia
-        //                daily_summary newday = new daily_summary();
-        //                newday.users = us;
-        //                newday.date = start;
-        //                if (lastSaved != null)
-        //                {
-        //                    newday.kcal_amount = lastSaved.kcal_amount;
-        //                    newday.weight = lastSaved.weight;
-        //                }
-        //                else
-        //                {
-        //                    newday.kcal_amount = 0;
-        //                    newday.weight = 0;
-        //                }
-
-        //                create((int)newday.weight, (int)newday.kcal_amount, us, null, newday.date);
-
-        //                start.AddDays(1);
-        //                break;
-
-        //            }
-
-        //            //jeeest
-        //            lastSaved = searchedsummary;
-
-        //            start.AddDays(1);
-
-        //        }
-        //    }
-        //}
     }
 }
---
+++ Dietaverse/DietaverseTests/Daily_SummaryTests.cs
@@ -0,0 +1,116 @@
+﻿using AutoRollbackExample;
+using Dietaverse.Database;
+using Dietaverse.Model;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DietaverseTests
+{
+    public class Daily_SummaryTests
+    {
+        [Fact]
+        [AutoRollback]
+        public void Create_CreateSucceed()
+        {
+            Users u = new Users();
+            string name = "testuser";
+            string password = "test";
+            double weight = 10;
+            users testuser = u.CreateAccount(name, weight, password);
+
+            Daily_summary summary = new Daily_summary();
+            summary.create(10, 10, testuser, "");
+            if(summary.downloadKcalFromDatabase(testuser, DateTime.Now)==10)
+            {
+                Assert.True(true);
+            }
+            if (summary.downloadWeightFromDatabase(testuser, DateTime.Now) == 10)
+            {
+                Assert.True(true);
+            }
+            if (summary.downloadNoteFromDatabase(testuser, DateTime.Now) == "")
+            {
+                Assert.True(true);
+            }
+        }
+        [Fact]
+        [AutoRollback]
+        public void Update_UpdateSucceed()
+        {
+            Users u = new Users();
+            string name = "testuser";
+            string password = "test";
+            double weight = 10;
+            users testuser = u.CreateAccount(name, weight, password);
+
+            Daily_summary summary = new Daily_summary();
+            summary.create(10, 10, testuser, "");
+            summary.update(10, 10, testuser, "note");
+            if (summary.downloadKcalFromDatabase(testuser, DateTime.Now) == 10)
+            {
+                Assert.True(true);
+            }
+            if (summary.downloadWeightFromDatabase(testuser, DateTime.Now) == 10)
+            {
+                Assert.True(true);
+            }
+            if (summary.downloadNoteFromDatabase(testuser, DateTime.Now) == "note")
+            {
+                Assert.True(true);
+            }
+        }
+
+        [Fact]
+        [AutoRollback]
+        public void downloadKcalFromDatabase_DownloadSucceed()
+        {
+            Users u = new Users();
+            string name = "testuser";
+            string password = "test";
+            double weight = 10;
+            users testuser = u.CreateAccount(name, weight, password);
+
+            Daily_summary summary = new Daily_summary();
+            summary.create(10, 10, testuser, "");
+
+
+            Assert.Equal(10, summary.downloadKcalFromDatabase(testuser, DateTime.Now));
+        }
+
+
+        [Fact]
+        [AutoRollback]
+        public void downloadWeightFromDatabase_DownloadSucceed()
+        {
+            Users u = new Users();
+            string name = "testuser";
+            string password = "test";
+            double weight = 10;
+            users testuser = u.CreateAccount(name, weight, password);
+
+            Daily_summary summary = new Daily_summary();
+            summary.create(10, 10, testuser, "");
+
+
+            Assert.Equal(10, summary.downloadWeightFromDatabase(testuser, DateTime.Now));
+        }
+
+        [Fact]
+        [AutoRollback]
+        public void downloadNoteFromDatabase_DownloadSucceed()
+        {
+            Users u = new Users();
+            string name = "testuser";
+            string password = "test";
+            double weight = 10;
+            users testuser = u.CreateAccount(name, weight, password);
+
+            Daily_summary summary = new Daily_summary();
+            summary.create(10, 10, testuser, "");
+
+
+            Assert.Equal("", summary.downloadNoteFromDatabase(testuser, DateTime.Now));
+        }
+    }
+}
